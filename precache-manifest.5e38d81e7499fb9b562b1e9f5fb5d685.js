bdd22fa040ee7ad27b45273def72e37dcc6723db
dhamie <dhamie.soyemi@gmail.com>
2020-10-23 00:48:28 +0100

    Updates

---
+++ precache-manifest.5e38d81e7499fb9b562b1e9f5fb5d685.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924dd0957bf5c561bc31f36ca6317ff7",
+    "url": "/reactpages/index.html"
+  },
+  {
+    "revision": "040a59062e9ac48bff6e",
+    "url": "/reactpages/static/css/main.0aad76a9.chunk.css"
+  },
+  {
+    "revision": "4ec09b3b55236c5a1229",
+    "url": "/reactpages/static/js/2.1e746556.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactpages/static/js/2.1e746556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040a59062e9ac48bff6e",
+    "url": "/reactpages/static/js/main.0f8f7fe9.chunk.js"
+  },
+  {
+    "revision": "44911be33e65eb57aa89",
+    "url": "/reactpages/static/js/runtime-main.2d2e84e8.js"
+  },
+  {
+    "revision": "4d092db6013445e856bd0b1b9f5e6b85",
+    "url": "/reactpages/static/media/KATO____.4d092db6.ttf"
+  },
+  {
+    "revision": "c275f6356ec25816d3e802f589df0120",
+    "url": "/reactpages/static/media/Okami-Outline.c275f635.otf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactpages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92e566504ab31424d1c32aebfa9cabbb",
+    "url": "/reactpages/static/media/samuria.92e56650.jpg"
+  },
+  {
+    "revision": "d481eb4d6c4e22455fe6272cfe1782ea",
+    "url": "/reactpages/static/media/yang_half.d481eb4d.png"
+  },
+  {
+    "revision": "6e75d46db8b6edb0c22d164e6d2aa5ab",
+    "url": "/reactpages/static/media/yin_half.6e75d46d.png"
+  }
+]);
